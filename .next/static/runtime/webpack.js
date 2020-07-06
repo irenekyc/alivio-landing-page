b87ffc78a6a768b47477ded9c335ccaf9a802b1d
irenekyc <gugu_irene@hotmail.com>
2020-07-06 15:31:39 +0200

    finish product section

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "add130ee1ecc36072245";
+/******/ 	var hotCurrentHash = "87194be922427427aa98";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Layout from "../layout/Layout";
 import Hero from "../components/Sections/Hero";
 import HowItWorks from "../components/Sections/HowItWorks";
 import SuccessStory from "../components/Sections/SuccessStory";
+import ProductSection from "../components/Sections/ProductSection";
 
 const Index = () => {
   return (
@@ -9,6 +10,7 @@ const Index = () => {
       <Hero />
       <HowItWorks />
       <SuccessStory />
+      <ProductSection />
     </Layout>
   );
 };
